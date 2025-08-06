66695549ec9fa9e4e3d2f1d2703e124afce5201e
meredwan <me.redwanislam@gmail.com>
2025-08-06 21:55:22 +0600

    Assignment 15 Done

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,19 +1,21 @@
-import 'package:assignment/screens/details_page.dart';
-import 'package:assignment/screens/sign_up.dart';
+import 'package:assignment/screens/Login_page.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<SignupPage> createState() => _SignupPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
   bool Isvisibility = true;
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passController = TextEditingController();
-  GlobalKey<FormState> _formkey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -25,30 +27,27 @@ class _LoginPageState extends State<LoginPage> {
             _buildStackImage(),
             _buildTextFild(),
             _LogingButton(),
-            Text(
-              "Forgot Password?",
-              style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),
-            ),
+
             SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupPage()),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Don't have an account?",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  Row(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Already have an Account",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Row(
                     children: [
                       Text(
                         " Login",
@@ -59,8 +58,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
@@ -71,11 +70,19 @@ class _LoginPageState extends State<LoginPage> {
   Widget _LogingButton() {
     return InkWell(
       onTap: () {
-        if (_formkey.currentState!.validate()) {
+        if (_formKey.currentState!.validate()) {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => DetailsPage()),
+            MaterialPageRoute(builder: (context) => LoginPage()),
           );
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              backgroundColor: Color.fromRGBO(143, 148, 251, 1),
+              content: Text('Sign Up Successfully'),
+              duration: Duration(seconds: 2),
+            ),
+          );
+          clearTextField();
         }
         return;
       },
@@ -94,7 +101,7 @@ class _LoginPageState extends State<LoginPage> {
         ),
         child: Center(
           child: Text(
-            "Login",
+            "Sign up",
             style: TextStyle(
               fontSize: 24,
               color: Colors.white,
@@ -108,7 +115,7 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _buildTextFild() {
     return Form(
-      key: _formkey,
+      key: _formKey,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Container(
@@ -128,6 +135,29 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               TextFormField(
                 autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _firstNameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Enter First Name';
+                  }
+                },
+                decoration: InputDecoration(hintText: "First Name"),
+              ),
+              Divider(thickness: 1),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _lastNameController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Enter Last Name';
+                  }
+                },
+                decoration: InputDecoration(hintText: "Last Name"),
+              ),
+              Divider(thickness: 1),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _emailController,
                 validator: (value) {
                   if (value!.isEmpty) {
                     return "Enter  Email";
@@ -139,35 +169,24 @@ class _LoginPageState extends State<LoginPage> {
                     return 'Enter a valid email with .com ';
                   }
                 },
-                controller: _emailController,
                 decoration: InputDecoration(hintText: "Email or Phone Number"),
               ),
-              Divider(thickness: 0.5),
+              Divider(thickness: 1),
               TextFormField(
                 autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _passController,
                 validator: (value) {
                   if (value!.isEmpty) {
                     return "Enter a Password";
                   }
-                  if (value!.length <6) {
+                  if (value!.length < 6) {
                     return "Enter a Password more than 6 Character";
                   }
                   return null;
                 },
-                controller: _passController,
-                obscureText: Isvisibility,
+                obscureText: true,
                 decoration: InputDecoration(
-                  suffixIcon: InkWell(
-                    onTap: () {
-                      setState(() {
-                        Isvisibility = !Isvisibility;
-                      });
-                    },
-                    child:
-                        Isvisibility
-                            ? Icon(Icons.visibility_off, color: Colors.grey)
-                            : Icon(Icons.visibility, color: Colors.grey),
-                  ),
+                  suffixIcon: Icon(Icons.visibility, color: Colors.grey),
                   hintText: "Password",
                 ),
               ),
@@ -227,9 +246,10 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void clearTextField(){
-    _passController.clear();
+  void clearTextField() {
+    _firstNameController.clear();
+    _lastNameController.clear();
     _emailController.clear();
+    _passController.clear();
   }
-
 }
