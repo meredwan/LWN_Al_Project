baa77f25a244608f02badb69586b7d10b37cb32b
meredwan <me.redwanislam@gmail.com>
2025-08-20 22:05:39 +0600

    Assignment 21 "add note with getx"

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,211 +1,95 @@
+import 'package:assignment/screens/favourite_screen.dart';
+import 'package:assignment/screens/note_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-import 'details_screens.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-/// Home Screen
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  List<Widget> pages = [NoteScreen(), FavouriteScreen()];
 
   @override
   Widget build(BuildContext context) {
-    final categories = ["All", "Combos", "Sliders", "Chicken"];
-    final burgers = [
-      {
-        "title": "Cheeseburger Wendy’s Burger",
-        "rating": 4.9,
-        "image": "assets/images/image 9.png",
-      },
-      {
-        "title": "Hamburger Veggie Burger",
-        "rating": 4.6,
-        "image": "assets/images/image 9.png",
-      },
-      {
-        "title": "Hamburger Chicken Burger",
-        "rating": 4.5,
-        "image": "assets/images/image 9.png",
-      },
-      {
-        "title": "Cheeseburger Wendy’s Burger",
-        "rating": 4.9,
-        "image": "assets/images/image 9.png",
-      },
-      {
-        "title": "Hamburger Veggie Burger",
-        "rating": 4.6,
-        "image": "assets/images/image 9.png",
-      },
-      {
-        "title": "Hamburger Chicken Burger",
-        "rating": 4.5,
-        "image": "assets/images/image 9.png",
-      },
-    ];
-
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "Foodgo",
-          style: GoogleFonts.lobster(textStyle: TextStyle(fontSize: 45)),
-        ),
-        actions: const [
-          CircleAvatar(backgroundImage: AssetImage("assets/user.png")),
-          SizedBox(width: 10),
+      appBar: AppBar(backgroundColor: Colors.brown, title: Text("Notes")),
+      body: Column(children: []),
+      bottomNavigationBar: GNav(
+        onTabChange: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        backgroundColor: Colors.brown,
+        activeColor: Colors.white,
+        color: Colors.grey,
+        selectedIndex: currentIndex,
+        iconSize: 24,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        tabs: [
+          GButton(icon: Icons.home, text: 'Home'),
+          GButton(icon: Icons.favorite_border, text: 'Likes'),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /// Search Bar
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
+      floatingActionButton: FloatingActionButton(
+        mini: false,
+        backgroundColor: Colors.brown,
+        onPressed: () {
+          _showAlertDialog(context);
+        },
+        child: Icon(Icons.add, color: Colors.white, size: 30),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  void _showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Center(
+          child: SingleChildScrollView(
+            child: AlertDialog(
+              title: Text("Create a Note"),
+              content: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "Enter a Title"),
+                  ),
+                  TextFormField(
                     decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.search),
-                      hintText: "Search",
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
+                      hintText: "Enter a Describetion",
                     ),
                   ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Text("Cancel"),
                 ),
-                SizedBox(width: 10),
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: Color(0XFFef2a39),
-                    borderRadius: BorderRadius.circular(22),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    foregroundColor: Colors.white,
                   ),
-                  child: Center(
-                    child: Icon(Icons.menu, color: Colors.white, size: 30),
-                  ),
+                  child: Text("Save"),
                 ),
               ],
             ),
-            const SizedBox(height: 15),
-
-            /// Categories
-            SizedBox(
-              height: 40,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 8),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: index == 0 ? Colors.red : Colors.grey[200],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: Text(
-                        categories[index],
-                        style: TextStyle(
-                          color: index == 0 ? Colors.white : Colors.black,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            const SizedBox(height: 15),
-
-            /// Burger Grid
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
-                  childAspectRatio: 0.8,
-                ),
-                itemCount: burgers.length,
-                itemBuilder: (context, index) {
-                  final burger = burgers[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => ProductScreen(burger: burger),
-                        ),
-                      );
-                    },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      elevation: 3,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset("assets/images/sp-1.png", height: 80),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "${burger["title"]}",
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text("⭐ ${burger["rating"]}"),
-                                Icon(Icons.favorite_border),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels: true,
-        selectedItemColor: Color(0XFFef2a39),
-        unselectedItemColor: Colors.grey,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            label: 'Favorite',
           ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        onPressed: () {},
-        child: const Icon(Icons.add, color: Colors.white, size: 35),
-      ),
+        );
+      },
     );
   }
 }
