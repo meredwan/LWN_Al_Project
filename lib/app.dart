9fc8dcd7634b9030c68f664b4f6ae1a1c9c71bf3
meredwan <me.redwanislam@gmail.com>
2025-08-20 22:05:39 +0600

    Assignment 21 Done

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,14 @@
 import 'package:assignment/screens/home_page.dart';
-import 'package:assignment/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-class foodgoApp extends StatelessWidget {
-  const foodgoApp({super.key});
+class NotepadApp extends StatelessWidget {
+  const NotepadApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
